00158336d2564e88b0677c9fdbf9a6d87bc3b07f
P0pini <popiksonek@gmail.com>
2025-02-25 18:55:45 +0100

    lab_1 Point

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("ABC!");
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("Liczba = " + i);
-        }
+        Point p1 = new Point();
+        p1.x = 5;
+        p1.y = 6;
+        System.out.println("Twój x to: " + p1.x + " a Twój y to: " + p1.y);
     }
 }
