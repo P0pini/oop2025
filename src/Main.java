f45eb756d3575d30e4e1e2b0ea0e6b87f9dc4aa3
P0pini <popiksonek@gmail.com>
2025-02-25 19:41:37 +0100

    lab_1 Point

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Point p1 = new Point();
-        p1.x = 5;
-        p1.y = 6;
+        p1.x = 250;
+        p1.y = 750;
         System.out.println("Twój x to: " + p1.x + " a Twój y to: " + p1.y);
+        System.out.println(p1);
+        System.out.println(p1.toSVG());
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,13 @@
 public class Point {
     public double x;
     public double y;
+    public String toString(){
+        return "x="+x+" y="+y;
+    }
+
+    public String toSVG(){
+        return "<svg height=\"1000\" width=\"1000\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+                "  <circle r=\"45\" cx=\""+x+"\" cy=\""+y+"\" fill=\"orange\" />\n" +
+                "</svg>";
+    }
 }
