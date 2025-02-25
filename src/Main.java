f65c664331d1cff22d38b279647f8c95453a30f4
P0pini <popiksonek@gmail.com>
2025-02-25 18:31:18 +0100

    lab_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {.
+    public static void main(String[] args) {
         System.out.printf("ABC!");
         for (int i = 1; i <= 5; i++) {
 
